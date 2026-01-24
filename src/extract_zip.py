51a43422445971d46a515e0457100ec5dcb5b6d3
JourneyCodesAyush <mithilesh71170@gmail.com>
2026-01-24 21:16:56 +0530

    feat(installer): install executables with hard links
    Extract executable files from package ZIPs and store them in
    packages/{pkg}/{version}/.exe. Create hard links in bin/ to simplify adding
    executables to PATH. If a hard link already exists, it is replaced, enabling
    future upgrade commands to point to new versions without affecting older
    packages.

---
+++ src/extract_zip.py
@@ -0,0 +1,69 @@
+import json
+import os
+import zipfile
+from pathlib import Path
+
+from . import result
+
+
+def extract_zip_file(install_result: result.InstallResult):
+
+    local_app_data = os.getenv("LOCALAPPDATA")
+    if not local_app_data:
+        install_result.success = False
+        install_result.error_message = "LOCALAPPDATA not set"
+
+        return install_result
+
+    package_folder = (
+        Path(local_app_data)
+        / ".ayuman"
+        / "packages"
+        / install_result.package_name
+        / install_result.version
+    )
+
+    bin_folder = Path(local_app_data) / ".ayuman" / "bin"
+
+    os.makedirs(package_folder, exist_ok=True)
+    os.makedirs(bin_folder, exist_ok=True)
+
+    metadata_json = package_folder / "metadata.json"
+    try:
+        with zipfile.ZipFile(install_result.zip_file_name, "r") as zip_ref:
+            for file_info in zip_ref.infolist():
+                if file_info.is_dir():
+                    continue
+                filename = Path(file_info.filename).name
+
+                if not filename.lower().endswith(".exe"):
+                    continue
+
+                target_path = package_folder / filename
+                hardlink_path = bin_folder / filename
+                with zip_ref.open(file_info) as source, open(
+                    target_path, "wb"
+                ) as target:
+                    target.write(source.read())
+
+                # Delete old hard link if it exists, would be handy while implementing 'upgrade'
+                if hardlink_path.exists():
+                    hardlink_path.unlink()
+
+                # Create new hard link
+                os.link(src=target_path, dst=hardlink_path)
+
+        with open(metadata_json, "w") as f:
+            json.dump(install_result.metadata, f)
+
+    except Exception as e:
+        install_result.success = False
+        install_result.error_message = str(e)
+        return install_result
+
+    install_result.install_path = str(package_folder)
+    install_result.success = True
+    install_result.error_message = None
+    install_result.metadata_path = str(metadata_json)
+
+    return install_result
