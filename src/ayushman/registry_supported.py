eeae8fc92cd85d7ca1793e0b782f8c2fbcbd60b6
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-04-10 10:47:36 +0530

    feat(validate): add new installable package 'sweep'

--- src/ayushman/registry_supported.py
+++ src/ayushman/registry_supported.py
@@ -1,6 +1,9 @@
 __all__ = ["SUPPORTED_PACKAGES"]
 
 SUPPORTED_PACKAGES: dict[str, dict] = {
+    "sweep": {
+        "description": "Recursively find and delete unwanted folders like node_modules"
+    },
     "cpp-cloc": {
         "description": "Count lines of code for various languages",
     },
