781bf9ef6a43d57c53f7d6f6b06b79022c43671e
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-07-07 17:10:48 +0530

    feat(constants): add filesystem configuration constants

--- src/ayushman/constants.py
+++ src/ayushman/constants.py
@@ -1,3 +1,8 @@
 __all__ = ["GITHUB_OWNER"]
 
 GITHUB_OWNER: str = "JourneyCodesAyush"
+
+INSTALL_DIR_NAME: str = "ayushman"
+PACKAGE_DIR_NAME: str = "packages"
+BIN_DIR_NAME: str = "bin"
+METADATA_FILE_NAME: str = "metadata"
